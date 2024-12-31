26c781dc821021de9e35df3169d62a9a3a59c682
Trịnh Đức Hoàng <124485427+DK625@users.noreply.github.com>
2024-12-31 12:06:56 +0700

    crud autotest

--- automation_test/offer.py
+++ automation_test/offer.py
@@ -155,8 +155,8 @@ class TestOffer:
 
     def login(self, username='admin', password='123123'):
         """Login to application"""
-        # self.page.goto("http://103.56.158.135:5173/login")
-        self.page.goto("http://localhost:5173/login")
+        self.page.goto("http://103.56.158.135:5173/login")
+        # self.page.goto("http://localhost:5173/login")
         self.page.fill("input[placeholder='Username']", username)
         self.page.fill("input[placeholder='Password']", password)
         self.page.click("button[type='submit']")
